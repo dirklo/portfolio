1928950ed6853f65cc977681c933a7730f35c740
Rick <dirklo@gmail.com>
2021-04-27 11:19:24 +0200

    add responsiveness for mobile

--- src/containers/title/TitleContainer.js
+++ src/containers/title/TitleContainer.js
@@ -18,12 +18,12 @@ export default function TitleContainer() {
         });
     }, []);
     return (
-        <div className="title_container slow">
+        <div className="title_container">
             <h1>Rick Moore</h1>
             <img src={mainPortrait} id="main-portrait" alt="main portrait" />
+            <div id="diamond"></div>
+            <div id="diamond2"></div>
             <div id="rect1"></div>
-            <div id="diamond" className='slow'></div>
-            <div id="diamond2" className='slower'></div>
             <SocialContainer />
             <IntroContainer />
             <NavBar />
