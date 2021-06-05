2a5f768adb5ec3a8cc74f8ef45d5c8972dd9df9a
Rick <dirklo@gmail.com>
2021-06-05 20:08:40 +0200

    fix blog card styling

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -2,12 +2,18 @@ import React from 'react'
 import './BlogCard.css'
 
 export default function BlogCard({ post }) {
+
+    function parseDate(date) {
+        const newDate = new Date(date)
+        return newDate.toDateString()
+    }
+
     return (
         <div className='blog-card'>
+            <h3>{parseDate(post.pubDate)}</h3>
             <h2>{post.title}</h2>
-            <h3>{post.pubDate.split(' ')[0]}</h3>
             <img src={post.thumbnail} alt={post.title}/>
-            <a href={post.link}>Read more...</a>
+            <a href={post.link} target='blank'>Read on Medium.com...</a>
         </div>
     )
 }
