8c6889f6603e708f983cf54f8c07ca9a3aceb06b
Rick <dirklo@gmail.com>
2021-06-06 16:14:33 +0200

    fix date bug

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import './BlogCard.css'
+import dayjs from 'dayjs'
 
 export default function BlogCard({ post }) {
 
-    function parseDate(date) {
-        const newDate = new Date(date)
-        return newDate.toDateString()
-    }
-
     return (
         <div className='blog-card'>
-            <h3>{parseDate(post.pubDate)}</h3>
+            <h3>{dayjs(post.pubDate).format('MMMM D, YYYY')}</h3>
             <h2>{post.title}</h2>
             <img src={post.thumbnail} alt={post.title}/>
             <a href={post.link} target='blank'>Read Full Article</a>
