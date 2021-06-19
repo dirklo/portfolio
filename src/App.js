7778290442ab52ddea169d1fc56ae66aea2fc283
Rick <dirklo@gmail.com>
2021-06-19 21:18:01 +0200

    add meta tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import TitleContainer from './containers/title/TitleContainer'
 import ProjectsContainer from './containers/projects/ProjectsContainer'
 import StoryContainer from './containers/story/StoryContainer'
@@ -12,6 +12,7 @@ import OnImagesLoaded from 'react-on-images-loaded'
 import NavBar from './components/NavBar'
 import NavMenu from './components/NavMenu'
 import BlogContainer from './containers/blog/BlogContainer';
+import MetaTags from 'react-meta-tags'
 
 const override = css(`
   display: block;
@@ -23,42 +24,46 @@ function App() {
   const [ loading, setLoading ] = useState(true)
   const [ hidePage, setHidePage ] = useState(false)
 
-  useEffect(() => { 
-    document.title = "Rick Moore Portfolio"
-  }, [])
-
   return (
-    <OnImagesLoaded 
-        onLoaded={() => setLoading(false)}
-      >
-      <div className="App">
-        {loading ?
-            <div className="loader">
-              <PropagateLoader color='gold' loading={loading} css={override} size={20} />
-            </div>
-          : null
-        }
-          <nav className="navbar-wrapper">
-            <NavBar loading={loading}/>
-          </nav>
-          <nav className="navmenu-wrapper">
-            <NavMenu setHidePage={(bool) => setHidePage(bool)} loading={loading}/>
-          </nav>
-          {
-            !hidePage ?
-              <>
-                <TitleContainer loading={loading} />
-                <VideoContainer loading={loading} />
-                <ProjectsContainer loading={loading}/>
-                <BlogContainer loading={loading} />
-                <StoryContainer loading={loading}/>
-                <ResumeContainer loading={loading}/>
-                <ContactContainer loading={loading}/>
-              </>
-            :null
+    <>
+      <MetaTags>
+              <title>Rick Moore Portfolio</title>
+              <meta name="description" content="Development Portfolio for Rick Moore." />
+              <meta property="og:title" content="Right Moore Portfolio" />
+              <meta property="og:image" content="./img/logo.png" />
+      </MetaTags>
+      <OnImagesLoaded 
+          onLoaded={() => setLoading(false)}
+        >
+        <div className="App">
+          {loading ?
+              <div className="loader">
+                <PropagateLoader color='gold' loading={loading} css={override} size={20} />
+              </div>
+            : null
           }
-      </div>
-    </OnImagesLoaded>
+            <nav className="navbar-wrapper">
+              <NavBar loading={loading}/>
+            </nav>
+            <nav className="navmenu-wrapper">
+              <NavMenu setHidePage={(bool) => setHidePage(bool)} loading={loading}/>
+            </nav>
+            {
+              !hidePage ?
+                <>
+                  <TitleContainer loading={loading} />
+                  <VideoContainer loading={loading} />
+                  <ProjectsContainer loading={loading}/>
+                  <BlogContainer loading={loading} />
+                  <StoryContainer loading={loading}/>
+                  <ResumeContainer loading={loading}/>
+                  <ContactContainer loading={loading}/>
+                </>
+              :null
+            }
+        </div>
+      </OnImagesLoaded>
+    </>
   )
 }
 
