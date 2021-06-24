30b74e0964f105fc2053635e74c2545816c113b7
Rick <dirklo@gmail.com>
2021-06-24 10:34:49 +0200

    remove meta from react index

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import MetaTags from 'react-meta-tags'
 
 ReactDOM.render(
   <React.StrictMode>
-    <MetaTags>
-              <title>Rick Moore Portfolio</title>
-              <meta name="description" content="Development Portfolio for Rick Moore" />
-              <meta property="og:title" content="Rick Moore Portfolio" />
-              <meta property="og:image" content="./img/logo.png" />
-    </MetaTags>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
