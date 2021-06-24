3983df01d9f779582323b2d3a6b4753d273c586b
Rick <dirklo@gmail.com>
2021-06-24 10:24:06 +0200

    add meta to index

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import MetaTags from 'react-meta-tags'
 
 ReactDOM.render(
   <React.StrictMode>
+    <MetaTags>
+              <title>Rick Moore Portfolio</title>
+              <meta name="description" content="Development Portfolio for Rick Moore" />
+              <meta property="og:title" content="Rick Moore Portfolio" />
+              <meta property="og:image" content="./img/logo.png" />
+    </MetaTags>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
